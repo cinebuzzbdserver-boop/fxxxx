32114c3bc504a0af285651ddf0e5542ee77eb3d0
cinebuzzbdserver-boop <cinebuzzbdserver@gmail.com>
2026-08-25 19:40:57 +0600

    Update CineFreakProvider.kt

--- plugins/CineFreakProvider.kt
+++ plugins/CineFreakProvider.kt
@@ -69,7 +69,7 @@ class CineFreakProvider : MainAPI() {
                             title = title,
                             image = image,
                             url = url,
-                            imageSize = "2:3"
+                            imageSize = "2:3" // p
                         )
                     )
                 }
